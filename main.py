a5d6104fc3e837d20d100b365a38a2cf745eb570
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2022-12-03 09:33:19 -0300

    Aplicação de orientação a objetos

--- main.py
+++ main.py
@@ -4,84 +4,96 @@ from openpyxl import load_workbook, Workbook
 import time
 
 
-def rpa_form():
-    browser = webdriver.Firefox()
+class RpaFerendum:
+    def __init__(self, researches, output_file):
+        self._researches = researches
+        self._output_file = output_file
+        self._researches_list = list()
 
-    data = get_data()
+    @property
+    def researches(self):
+        return self._researches
 
-    link_list = list()
-    for row in data:
-        browser.get('https://ferendum.com/pt/')
-        fill_form(browser, row)
-        link_search, link_admin = send_form(browser)
-        link_list.append((link_search, link_admin))
+    @researches.setter
+    def researches(self, researches):
+        self._researches = researches
+
+    @property
+    def output_file(self):
+        return self._output_file
+
+    @output_file.setter
+    def output_file(self, output_file):
+        self._output_file = output_file
+
+    def run(self):
+        browser = webdriver.Firefox()
+
+        data = self._get_data()
+
+        self._researches_list = list()
+        for row in data:
+            browser.get('https://ferendum.com/pt/')
+            self._fill_form(browser, row)
+            link_search, link_admin = self._send_form(browser)
+            self._researches_list.append((link_search, link_admin))
+            time.sleep(1)
+
+    def _get_data(self):
+        wb = load_workbook(filename=self._researches)
+        sheet = wb['Pesquisas']
+        data = list()
+        for row in sheet:
+            row_values = [cell.value for cell in row]
+            row_values[3] = [v.strip() for v in row_values[3].split(',')]
+            row_values[4:] = [0 if v == 'Sim' else 1 for v in row_values[4:]]
+            data.append(row_values)
+        return data[1:]
+
+    def get_researches_list(self):
+        return self._researches_list
+
+    def save(self):
+        wb = Workbook()
+        ws1 = wb.active
+        ws1.title = 'Links das pesquisas'
+
+        for row, search in enumerate(self._researches_list):
+            ws1.cell(row=row + 1, column=1, value=search[0])
+            ws1.cell(row=row + 1, column=2, value=search[1])
+
+        wb.save(filename=self._output_file)
+
+    @staticmethod
+    def _fill_form(driver, data):
+        driver.find_element(By.NAME, 'titulo').send_keys(data[0])
+        driver.find_element(By.NAME, 'descripcion').send_keys(data[1])
+        driver.find_element(By.NAME, 'creador').send_keys(data[2])
+        for i, op in enumerate(data[3]):
+            driver.find_element(By.ID, f'op{i + 1}').send_keys(op)
+        driver.find_elements(By.NAME, 'config_anonimo')[data[4]].click()
+        driver.find_elements(By.NAME, 'config_priv_pub')[data[5]].click()
+        driver.find_elements(By.NAME, 'config_un_solo_voto')[data[6]].click()
+        driver.find_elements(By.NAME, 'config_aut_req')[data[7]].click()
+        driver.find_element(By.NAME, 'accept_terms_checkbox').click()
+
+    @staticmethod
+    def _send_form(driver):
+        driver.find_element(By.CLASS_NAME, 'btn-primary').click()
+
         time.sleep(1)
 
-    save_links(link_list)
+        driver.find_element(By.CLASS_NAME, 'btn-primary').click()
 
+        link_search = driver.find_element(By.ID, 'textoACopiar').get_property('href')
+        link_admin = driver\
+            .find_element(By.CSS_SELECTOR, 'div.card:nth-child(1) > div:nth-child(1) > a:nth-child(22)')\
+            .get_property('href')
 
-def get_data():
-    wb = load_workbook(filename='dados.xlsx')
-    sheet = wb['Pesquisas']
-    data = list()
-    row = 1
-    row_data = list()
-    for key, cell in sheet._cells.items():
-        if key[0] != row:
-            data.append(row_data)
-            row_data = list()
-            row = key[0]
-
-        if key[1] == 4:
-            row_data.append([v.strip() for v in cell.value.split(',')])
-        elif key[1] >= 5:
-            row_data.append(0 if cell.value == 'Sim' else 1)
-        else:
-            row_data.append(cell.value)
-
-    data.append(row_data)
-    return data[1:]
-
-
-def save_links(link_list, filename='links.xlsx'):
-    wb = Workbook()
-    ws1 = wb.active
-    ws1.title = 'Links das pesquisas'
-
-    for row, links in enumerate(link_list):
-        ws1.cell(row=row + 1, column=1, value=links[0])
-        ws1.cell(row=row + 1, column=2, value=links[1])
-
-    wb.save(filename=filename)
-
-
-def fill_form(driver, data):
-    driver.find_element(By.NAME, 'titulo').send_keys(data[0])
-    driver.find_element(By.NAME, 'descripcion').send_keys(data[1])
-    driver.find_element(By.NAME, 'creador').send_keys(data[2])
-    for i, op in enumerate(data[3]):
-        driver.find_element(By.ID, f'op{i + 1}').send_keys(op)
-    driver.find_elements(By.NAME, 'config_anonimo')[data[4]].click()
-    driver.find_elements(By.NAME, 'config_priv_pub')[data[5]].click()
-    driver.find_elements(By.NAME, 'config_un_solo_voto')[data[6]].click()
-    driver.find_elements(By.NAME, 'config_aut_req')[data[7]].click()
-    driver.find_element(By.NAME, 'accept_terms_checkbox').click()
-
-
-def send_form(driver):
-    driver.find_element(By.CLASS_NAME, 'btn-primary').click()
-
-    time.sleep(1)
-
-    driver.find_element(By.CLASS_NAME, 'btn-primary').click()
-
-    link_search = driver.find_element(By.ID, 'textoACopiar').get_property('href')
-    link_admin = driver\
-        .find_element(By.CSS_SELECTOR, 'div.card:nth-child(1) > div:nth-child(1) > a:nth-child(22)')\
-        .get_property('href')
-
-    return link_search, link_admin
+        return link_search, link_admin
 
 
 if __name__ == '__main__':
-    rpa_form()
+    rpa = RpaFerendum(researches='dados.xlsx', output_file='links.xlsx')
+    rpa.run()
+    rpa.save()
